0c94f5c1e4175d31d3b8a5f00046f674264f3c24
NidaaABoulkacem <naboulkacem1@nait.ca>
2023-05-28 21:18:33 -0600

    saving

--- dmit2015-faces-firebase-demo/src/test/java/domain/ChineseZodiacTest.java
+++ dmit2015-faces-firebase-demo/src/test/java/domain/ChineseZodiacTest.java
@@ -2,11 +2,60 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChineseZodiacTest {
 
+    public static final int CURRENT_YEAR = Calendar.getInstance().get(Calendar.YEAR);
     @Test
-    void animal() {
+    public void testGetAnimalRat() {
+        int animalYear = 1900;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("rat", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
+    }
+    @Test
+    public void testGetAnimalOx() {
+        int animalYear = 1901;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("ox", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
+    }
+    @Test
+    public void testGetAnimalTiger() {
+        int animalYear = 1902;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("tiger", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
+    }
+
+    @Test
+    public void testGetAnimalRabbit() {
+        int animalYear = 1903;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("rabbit", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
+    }
+    @Test
+    public void testGetAnimalDragon() {
+        int animalYear = 1904;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("dragon", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
+    }
+    @Test
+    public void testGetAnimalSnake() {
+        int animalYear = 1905;
+        while (animalYear <= CURRENT_YEAR) {
+            assertEquals("snake", ChineseZodiac.animal( animalYear ).toLowerCase() );
+            animalYear += 12;
+        }
     }
 }
